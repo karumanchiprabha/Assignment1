801a4e3eb2c9bdcbd38b3b45d748f27f121c6b1d
karumanchiprabha <115642859+karumanchiprabha@users.noreply.github.com>
2022-10-31 14:00:48 +0530

    Update Pattern5.java

--- Pattern5.java
+++ Pattern5.java
@@ -3,47 +3,22 @@ public class Pattern5
     public static void main(String[] args)
     {
 
-        for (int i = 0; i <14; i++)
+        int i, j, n=13;
+        for (i = 0; i<n; i++)
         {
-            for (int j = 0; j <14; j++)
+            for (j = 0; j <n; j++)
             {
-                if(i==0)
+                if(i==0||i==n-1||j==0||i==1&&j<n/2||i==n-2&&j<n/2||i==2&&j<=i+(n-j-1)/2||i==3&&j<(n/2)-2 ||i==4&&j<(n/2)-3||
+                  i==5&&j<(n/2)-4||i==6&&j<(n/2)-5||i==n-3&&j<(n/2)-1||i==n-4&&j<(n/2)-2||i==n-5&&j<(n/2)-3||i==n-6&&j<(n/2)-4)
                 {
                     System.out.print("*");
                 }
-            }
-        }
-        System.out.println();
-        int i, j, n=6;
-//Prints upper half  pattern
-        for (i = n; i >= 1; i--)
-        {
-            for (j = 1; j <= i; j++)
-            {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-//Prints lower half  pattern
-        for (i = 2; i <= n; i++)
-        {
-            for (j = 1; j <= i; j++)
-            {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        for ( i = 0; i <14 ; i++)
-        {
-            for (j = 0; j <14; j++)
-            {
-                if(i==n-1)
+                else
                 {
-                    System.out.print("*");
+                    System.out.print(" ");
                 }
             }
+            System.out.println();
         }
-        System.out.println();
     }
 }
-
